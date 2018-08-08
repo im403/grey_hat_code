b101c291ec3cfc725433b7a8d4da0c523fe3c23d
praveenbhuviceh <praveenbhuvi2328@gmail.com>
2018-08-08 14:41:06 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,13 @@
+n=input()
+if(n=='a' or n=='A'):
+    print("Vowel")
+elif(n=='e' or n=='E'):
+    print("vowel")
+elif(n=='i' or n=='I'):
+    print("vowel")
+elif(n=='o' or n=='O'):
+    print("vowel")
+elif(n=='u' or n=='U'):
+    print("vowel")
+else:
+    print("consonants")
